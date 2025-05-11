288f69bcdf1174aedd52a64f6737ee7e8a42aab1
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-05-11 07:11:42 +0000

    テスト中修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_05_08_064617) do
+ActiveRecord::Schema.define(version: 2025_05_08_063933) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
