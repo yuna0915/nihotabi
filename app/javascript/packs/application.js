847ca7fb8288fc1d7b9319d333318c3d32e3a90f
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-05-16 14:43:53 +0000

    トップページ画像編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,24 @@ import "../script";
 
 // Webpackに画像を認識させる
 const images = require.context('../images', true, /\.(png|jpe?g|svg)$/);
+
+// ▼ トップページの背景画像切り替え処理
+document.addEventListener('turbolinks:load', () => {
+  const el = document.querySelector('.homes-top');
+  if (!el) return;
+
+  const bgImages = [
+    images('./top1.jpg'),
+    images('./top2.jpg'),
+    images('./top3.jpg'),
+    images('./top4.jpg')
+  ];
+
+  let i = 0;
+  el.style.backgroundImage = `url(${bgImages[i]})`;
+
+  setInterval(() => {
+    i = (i + 1) % bgImages.length;
+    el.style.backgroundImage = `url(${bgImages[i]})`;
+  }, 5000);
+});
