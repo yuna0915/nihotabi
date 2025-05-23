a7420efd330dca19cee842479acee9865bbc4dac
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-05-23 16:58:08 +0000

    notified_usersテーブル作成のためのマイグレーション追加2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_05_22_090749) do
+ActiveRecord::Schema.define(version: 2025_05_23_164515) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -116,6 +116,13 @@ ActiveRecord::Schema.define(version: 2025_05_22_090749) do
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
+  create_table "notified_users", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notified_users_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "location_genre_id", null: false
@@ -207,6 +214,7 @@ ActiveRecord::Schema.define(version: 2025_05_22_090749) do
   add_foreign_key "notifications", "posts"
   add_foreign_key "notifications", "users"
   add_foreign_key "notifications", "users", column: "notified_user_id"
+  add_foreign_key "notified_users", "users"
   add_foreign_key "posts", "location_genres"
   add_foreign_key "posts", "prefectures"
   add_foreign_key "posts", "users"
