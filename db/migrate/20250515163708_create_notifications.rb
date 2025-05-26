a75524a8701392175645e14f1077a70208cc352a
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-05-26 20:10:38 +0000

    テスト中修正3

--- db/migrate/20250515163708_create_notifications.rb
+++ db/migrate/20250515163708_create_notifications.rb
@@ -2,13 +2,13 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :notified_user, null: false, foreign_key: true
+      t.references :notified_user, null: false, foreign_key: { to_table: :users }
       t.references :post, null: false, foreign_key: true
       t.references :comment, null: false, foreign_key: true
       t.string :action
       t.boolean :checked, default: false, null: false
-
+    
       t.timestamps
-    end
+    end    
   end
 end
