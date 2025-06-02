baaa4e9df889edf8c8e0267413e02755a411ccaa
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-06-02 09:38:50 +0000

    IPアドレスからのアクセスでも画面が表示されるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
+  # セキュリティ対策（不正なフォーム送信を防ぐ）
+  protect_from_forgery with: :exception
   # Deviseの追加許可パラメータ設定
   before_action :configure_permitted_parameters, if: :devise_controller?
   # 管理者とユーザーの同時ログイン防止
   before_action :sign_out_conflicting_resource
+  # IPアドレスで直接アクセスしても、画面が表示されるようにする
+  before_action :allow_http_origin_for_ip
 
   protected
 
@@ -29,6 +33,13 @@ class ApplicationController < ActionController::Base
       sign_out(:user)  # 管理ページではユーザーをログアウト
     elsif !controller_path.start_with?("admin/") && admin_signed_in?
       sign_out(:admin) # ユーザーページでは管理者をログアウト
+    end
+  end
+
+  # IPアドレスでアクセスされたときに、画面の表示エラーを防ぐための設定
+  def allow_http_origin_for_ip
+    if request.remote_ip == '13.230.105.193' || request.host == '13.230.105.193'
+      response.headers['X-CSRF-Token'] = form_authenticity_token
     end
   end
 end
